437a0c370c4d2b18fb3ccbc0820ff3558e83a02e
benjamin <benjaminrenny@gmail.com>
2026-01-09 16:47:24 -0800

    user routes and controllers added

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-import cloudinary from "../config/cloudinary.config.js";
+import {cloudinary} from "../config/cloudinary.js";
 import { Product } from "../models/product.model.js";
 import { Order } from "../models/order.model.js";
 import { User } from "../models/user.model.js";
--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import { functions, inngest } from "./config/inngest.js";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import adminRoutes from "./routes/admin.route.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 
@@ -15,7 +16,9 @@ app.use(express.json())
 app.use(clerkMiddleware());
 
 app.use('/api/inngest', serve({client: inngest, functions}));
+
 app.use('/api/admin', adminRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/api/health', (req,res) => {
     res.status(200).json({message:"success"});
